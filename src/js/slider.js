48ced080dc3a27eb9b34190fabef246b92ea4343
Yaroslav Grabovych <ygrabovych@gmail.com>
2023-03-27 22:01:34 +0300

    js breakpoints

--- src/js/slider.js
+++ src/js/slider.js
@@ -3,6 +3,23 @@ const swiper = new Swiper('.swiper', {
   direction: 'horizontal',
   loop: true,
   slidesPerView: 1,
+  breakpoints: {
+    // when window width is >= 320px
+    428: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+    // when window width is >= 480px
+    768: {
+      slidesPerView: 2.7,
+      spaceBetween: 30
+    },
+    // when window width is >= 640px
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 40
+    }
+  },
 
   // If we need pagination
   pagination: {
