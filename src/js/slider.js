bbda0b490b80bf9ea8bf03c932d97ea2b57d6ae5
Metaldetect <stas.klymenko@icloud.com>
2023-03-27 16:25:48 +0200

    Update slider.js

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,13 +1,29 @@
 const swiper = new Swiper('.swiper', {
   // Optional parameters
-  direction: 'vertical',
+  direction: 'horizontal',
   loop: true,
 
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
   },
-
+  speed: 1500,
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+    },
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 2,
+    },
+    // when window width is >= 1200px
+    1200: {
+      slidesPerView: 3,
+    },
+  },
   // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
