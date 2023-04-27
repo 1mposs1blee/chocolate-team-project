8b37fe4380a8d27c07afe7e201a1419e01870d8e
Yaroslav337 <pelerovteatr@gmail.com>
2023-04-27 22:59:48 +0300

    fix modal-buy__swiper

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import './js/mobile-menu';
 import './js/modal';
+import './js/modal-buy-swiper';
 import './js/our-products-wiper';
 import './js/reviews-swiper';
 import './js/how-its-mate--video';
